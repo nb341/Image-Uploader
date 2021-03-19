14d6acf39762120fc5692c73043f60e6f8318f6d
nb341 <narindrab4@gmail.com>
2021-03-19 10:12:40 -0400

    heroku post scripts

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const multer = require('multer');
 const path = require('path');
 const app = express();
 const cors = require('cors');
-const port = 5000;
+const port = process.env.PORT || 5000;;
 
-const baseUrl = "http://localhost:5000/";
+const baseUrl = "";
 
 app.use(express.static(path.join(__dirname, 'public')));
 
