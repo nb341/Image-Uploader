a8241c627adf01a2ee873616fd9c3cdbad75f159
nb341 <narindrab4@gmail.com>
2021-12-25 10:22:02 -0400

    refactored image uploader to functional component

--- client/src/components/ImageUploader.js
+++ client/src/components/ImageUploader.js
@@ -1,81 +1,77 @@
-import React, {Component} from 'react';
+import React, {useRef, useEffect} from 'react';
 import bgSVG from '../assets/image.svg';
 import DropZone from './DropZone';
 
-class ImageUploader extends Component {
-    constructor(props){
-        super(props);
-    }
 
-    selectRef = React.createRef();
 
-    onFileChange = event => {
+function ImageUploader(props){
+  const selectRef = useRef();
+  const onFileChange = event => {
     
-        // Update the state
-        event.preventDefault();
-        const files = event.target.files
-        console.log(files[0])
-        const formData = new FormData();
-        formData.append('image', files[0]);
-        console.log(formData.get('image'))
-        this.props.uploadView();
-        fetch('postImage/', {
-            method: 'POST',
-            mode: "cors",
-            cache: "no-cache", 
-            credentials: "same-origin",
-            body: formData
-          })
-          .then(response => response.json())
-          .then(data => {
-            console.log(data.err + "Response Here")
-            if(data.err) this.props.selectView();
-            else{
-              
-              console.log("Look for this "+data.url)
-              console.log(typeof data.url)
-            this.props.uploadedView(data.url);
-            
-            }
-          })
-          .catch(error => {
-            console.error(error);
-            this.props.selectView();
-          })
-
-      }
-
-     
+    // Update the state
+    event.preventDefault();
+    const files = event.target.files
+    console.log(files[0])
+    const formData = new FormData();
+    formData.append('image', files[0]);
+    console.log(formData.get('image'))
+    props.uploadView();
+    fetch('postImage/', {
+        method: 'POST',
+        mode: "cors",
+        cache: "no-cache", 
+        credentials: "same-origin",
+        body: formData
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data.err + "Response Here")
+        if(data.err) props.selectView();
+        else{
           
-      
+          console.log("Look for this "+data.url)
+          console.log(typeof data.url)
+        props.uploadedView(data.url);
+        
+        }
+      })
+      .catch(error => {
+        console.error(error);
+        props.selectView();
+      })
 
-    componentDidMount(){
-        let input = this.selectRef.current;
-        input.addEventListener('change', this.onFileChange);
+  }
 
+
+
+  useEffect(()=>{
+    const input = selectRef.current;
+    input.addEventListener('change', onFileChange);
+
+    return ()=>{
+      //let input = selectRef.current;
+      input.removeEventListener('change');
     }
-    componentWillUnmount(){
-        let input = this.selectRef.current;
-        input.removeEventListener('change', this.onFileChange);
-    }
-   
-    render(){
-        return(
+  }, [selectRef, onFileChange])
+
+    return(
             <div className="upload-container">
-            <h1 className="header">Upload your image</h1>
-            <p className="file-type">File should be Jpeg, Png...</p>
-            <DropZone fileChange={this.onFileChange} uploadedView={this.props.uploadedView} selectView = {this.props.selectView} uploadView={this.props.uploadView}>
-              <img className="img" src={bgSVG} alt="mountain behind clouds"/>
-              <p className="drop-text">Drag & Drop your image here</p>
+              <h1 className="header">Upload your image</h1>
+              <p className="file-type">File should be Jpeg, Png...</p>
+              <DropZone fileChange={onFileChange} uploadedView={props.uploadedView} selectView = {props.selectView} uploadView={props.uploadView}>
+                <img className="img" src={bgSVG} alt="mountain behind clouds"/>
+                <p className="drop-text">Drag & Drop your image here</p>
               </DropZone>
-            <p className="or">Or</p>
-            <label className="select-file">
-            <input type="file" onClick={(e)=>{console.log("waddup")}} ref={this.selectRef} className="select-file"/>
-            Choose file
-            </label>
-        </div>
-        );
-    }
+              <p className="or">Or</p>
+              <label className="select-file">
+              <input type="file" onClick={(e)=>{console.log("waddup")}} ref={selectRef} className="select-file"/>
+                Choose file
+              </label>
+            </div>
+          );
 }
+
+
+
 
 export default ImageUploader;
